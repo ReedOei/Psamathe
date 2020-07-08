a5e18ab4f394333c3463fc166b4dd5fb6dc95ca7
Reed Oei <oei.reed@gmail.com>
2020-07-08 11:02:54 -0500

    Improve scripts to track more stuff

--- scripts/analyze-external-calls.py
+++ scripts/analyze-external-calls.py
@@ -4,6 +4,20 @@ from collections import Counter
 
 from tqdm import tqdm
 
+def aggregate_counter(results, counter_name, counter):
+    counter_name_unique = counter_name + '_unique'
+    counter_name_uses = counter_name + '_uses'
+
+    if not counter_name in results:
+        results[counter_name] = Counter()
+        results[counter_name_unique] = Counter()
+        results[counter_name_uses] = 0
+
+    results[counter_name] += counter
+    for k in counter.items():
+        results[counter_name_unique][k] += 1
+    results[counter_name_uses] += 1 if len(counter) > 1 else 0
+
 def main(args):
     if len(args) < 2:
         print("Usage: python3 analyze-external-calls.py RESULTS_FILE")
@@ -12,9 +26,21 @@ def main(args):
     fname = args[1]
 
     total = 0
-    successes = 0
+
+    counts = {
+        'success': 0,
+        'writes_after_internal_call': 0,
+        'writes_after_internal_state_change_call': 0,
+        'writes_after_external_call': 0,
+        'writes_after_non_library_call': 0,
+        'reads_after_internal_call': 0,
+        'reads_after_internal_state_change_call': 0,
+        'reads_after_external_call': 0,
+        'reads_after_non_library_call': 0
+    }
+
     results = {
-        'non_library_call': Counter()
+        'counts': counts
     }
 
     uses_external_calls = 0
@@ -29,29 +55,35 @@ def main(args):
 
             total += 1
 
-            if res['success']:
-                successes += 1
+            for k in counts:
+                if res[k]:
+                    counts[k] += 1
 
             for k in res:
                 if isinstance(res[k], dict):
                     res[k] = Counter(res[k])
-                    if not k in results:
-                        results[k] = Counter()
-                        results[k + '_unique'] = Counter()
-                        results[k + '_uses'] = 0
+                    aggregate_counter(results, k, res[k])
 
-                    results[k] += res[k]
-                    for call in res[k].items():
-                        results[k + '_unique'][call] += 1
-                    results[k + '_uses'] += 1 if len(res[k]) > 1 else 0
+                    # It only makes sense to do this part for the Counter that count (contract, function) pairs, because
+                    # we need to know if the call is being made to the SafeMath library
+                    if k.endswith('call'):
+                        non_safe_math_calls = Counter({(con, func): count for (con, func), count in res['external_call'].items() if con != 'SafeMath'})
+                        aggregate_counter(results, 'non_safe_math_call', non_safe_math_calls)
 
             non_library_calls = res['external_call'] - res['library_call']
-            results['non_library_call'] += non_library_calls
+            aggregate_counter(results, 'non_library_call', non_library_calls)
 
-    print(results)
-    print('Success: {} of {}'.format(successes, total))
-    print('Uses external calls: {}'.format(results['uses_external_call']))
-    print('Uses internal calls: {}'.format(results['uses_internal_call']))
+    # print(results)
+    print('Counts ({} total): {}'.format(total, counts))
+    print('Uses external calls: {}'.format(results['external_call_uses']))
+    print('Uses non-library calls: {}'.format(results['non_library_call_uses']))
+    print('Uses non-SafeMath calls: {}'.format(results['non_safe_math_call_uses']))
+    print('Uses internal calls: {}'.format(results['internal_call_uses']))
+    print('Uses internal state change calls: {}'.format(results['internal_state_change_call_uses']))
+
+    for k in results:
+        if isinstance(results[k], Counter):
+            print(k, ':', results[k].most_common(10))
 
 if __name__ == '__main__':
     main(sys.argv)
--- scripts/external-calls.py
+++ scripts/external-calls.py
@@ -1,12 +1,55 @@
 import sys
 from slither.slither import Slither
+from slither.exceptions import SlitherError
 
 from collections import Counter
 
 def record_call_set(res, call_set_name, it):
     for con, func in [(str(con), str(func)) for con, func in it]:
-        res[call_set_name + '_func'][func] += 1
-        res[call_set_name + '_call'][(con, func)] += 1
+        # require(E) doesn't really count as an internal call...
+        if not 'require(bool)' in func:
+            res[call_set_name + '_func'][func] += 1
+            res[call_set_name + '_call'][(con, func)] += 1
+
+def explore_call_usage(calls, f, found_call, explored, node):
+    if node is None:
+        return False
+
+    if node in explored:
+        return False
+
+    if found_call and len(f(node)) > 0:
+        return True
+
+    if not found_call and len(calls(node)) > 0:
+        found_call = True
+        # Reset the explored list because we now know that everything after could be after a call
+        explored = [node]
+    else:
+        explored = explored + [node]
+
+    for next_node in node.sons:
+        if explore_call_usage(calls, f, found_call, explored, next_node):
+            return True
+
+    return False
+
+def do_explore(calls, f, node):
+    return explore_call_usage(calls, f, False, [], node)
+
+def external_calls(node):
+    return node.high_level_calls
+
+def internal_calls(node):
+    return node.internal_calls
+
+def internal_state_change_calls(state_changing_func_names):
+    def f(node):
+        return [func for func in node.internal_calls if func.name in state_changing_func_names]
+    return f
+
+def non_library_calls(node):
+    return list(set(node.high_level_calls) - set(node.library_calls))
 
 def analyze(fname):
     res = {
@@ -17,6 +60,16 @@ def analyze(fname):
         'internal_call': Counter(),
         'library_func': Counter(),
         'library_call': Counter(),
+        'internal_state_change_call': Counter(),
+        'internal_state_change_func': Counter(),
+        'writes_after_internal_call': False,
+        'writes_after_internal_state_change_call': False,
+        'writes_after_external_call': False,
+        'writes_after_non_library_call': False,
+        'reads_after_internal_call': False,
+        'reads_after_internal_state_change_call': False,
+        'reads_after_external_call': False,
+        'reads_after_non_library_call': False,
         'success': False
     }
 
@@ -26,17 +79,29 @@ def analyze(fname):
         for c in slither.contracts:
             # print('Contract: {}'.format(c.name))
 
+            state_changing_func_names = set(func.name for func in c.functions if len(func.all_state_variables_written()) > 0)
+
             for function in c.functions:
                 # print('Function: {}'.format(function.name))
 
                 record_call_set(res, 'external', function.all_high_level_calls())
                 record_call_set(res, 'internal', [(c, f) for f in function.all_internal_calls()])
+                record_call_set(res, 'internal_state_change', [(c, f) for f in function.all_internal_calls() if f.name in state_changing_func_names])
                 record_call_set(res, 'library', function.all_library_calls())
+
+                res['writes_after_internal_call'] |= do_explore(internal_calls, lambda n: n.state_variables_written, function.entry_point)
+                res['writes_after_internal_state_change_call'] |= do_explore(internal_state_change_calls(state_changing_func_names), lambda n: n.state_variables_written, function.entry_point)
+                res['writes_after_external_call'] |= do_explore(external_calls, lambda n: n.state_variables_written, function.entry_point)
+                res['writes_after_non_library_call'] |= do_explore(non_library_calls, lambda n: n.state_variables_written, function.entry_point)
+
+                res['reads_after_internal_call'] |= do_explore(internal_calls, lambda n: n.state_variables_read, function.entry_point)
+                res['reads_after_internal_state_change_call'] |= do_explore(internal_state_change_calls(state_changing_func_names), lambda n: n.state_variables_read, function.entry_point)
+                res['reads_after_external_call'] |= do_explore(external_calls, lambda n: n.state_variables_read, function.entry_point)
+                res['reads_after_non_library_call'] |= do_explore(non_library_calls, lambda n: n.state_variables_read, function.entry_point)
 
                 # print('External calls: {}'.format(external_calls))
         res['success'] = True
-    except Exception as e:
-        # raise e
+    except SlitherError as e:
         pass
 
     for k in res:
