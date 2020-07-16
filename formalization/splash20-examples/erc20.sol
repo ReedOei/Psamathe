56a2deb5e9bcefb87b4c090f2d1e0e25dfec6b79
Reed Oei <oei.reed@gmail.com>
2020-07-16 12:34:54 -0500

    Add syntax figure

--- formalization/splash20-examples/erc20.sol
+++ formalization/splash20-examples/erc20.sol
@@ -5,7 +5,6 @@ contract EIP20 is EIP20Interface {
     event Approval(address _owner, address _spender, uint256 _value);
     mapping (address => uint256) public balances;
     mapping (address => mapping (address => uint256)) public allowed;
-
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balances[msg.sender] >= _value);
         balances[msg.sender] -= _value;
@@ -13,7 +12,6 @@ contract EIP20 is EIP20Interface {
         emit Transfer(msg.sender, _to, _value);
         return true;
     }
-
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         uint256 allowance = allowed[_from][msg.sender];
         require(balances[_from] >= _value && allowance >= _value);
@@ -22,19 +20,9 @@ contract EIP20 is EIP20Interface {
         emit Transfer(_from, _to, _value);
         return true;
     }
-
-    function balanceOf(address _owner) public view returns (uint256 balance) {
-        return balances[_owner];
-    }
-
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
     }
-
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
-        return allowed[_owner][_spender];
-    }
 }
-
