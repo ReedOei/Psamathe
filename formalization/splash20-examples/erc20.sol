f467ac449d5d43ec5ff106455dfc515ba207da38
Reed Oei <oei.reed@gmail.com>
2020-07-21 08:21:06 -0500

    Some simplications to examples

--- formalization/splash20-examples/erc20.sol
+++ formalization/splash20-examples/erc20.sol
@@ -1,14 +1,6 @@
 contract EIP20 {
-    uint256 totalSupply;
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
-    function transfer(address to, uint256 value)
-        public returns (bool success) {
-        require(balances[msg.sender] >= value);
-        balances[msg.sender] -= value;
-        balances[to] += value;
-        return true;
-    }
     function transferFrom(address from, address to, uint256 value)
         public returns (bool success) {
         require(balances[from] >= value &&
