99af1e6a43dff8000402bdc9736928db9a09e0a2
Reed Oei <oei.reed@gmail.com>
2020-07-21 16:21:39 -0500

    Add very basic compiler.

--- scripts/save-test-output.sh
+++ scripts/save-test-output.sh
@@ -2,15 +2,8 @@
 
 test_dir="$1"
 shift
-rerun_all="$1"
-shift
 
 for fname in $(find "$test_dir" -name "*.flow" -type f); do
-    if [[ -e "$fname.out" ]] && [[ -z "$rerun_all" ]]; then
-        echo "[INFO] Skipping $fname, output file already exists."
-        continue
-    fi
-
     "$(git rev-parse --show-toplevel)/flow" "$@" "$fname" | tee "$fname.out"
 done
 
