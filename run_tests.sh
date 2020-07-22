efc06481e77398fe8e846f0bb2207836866b9b64
Reed Oei <oei.reed@gmail.com>
2020-07-21 20:48:46 -0500

    Added missing run_tests script

---
+++ run_tests.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+fast="false"
+
+if [[ "$1" == "--fast" ]]; then
+    shift
+    fast="true"
+fi
+
+kserver="$HOME/k-framework/k-distribution/bin/kserver"
+
+set -x
+
+pkill -f "kserver"
+
+"$kserver" &> /dev/null &
+kserver_pid=$!
+if [[ "$fast" == "false" ]]; then
+    # Warmup
+    make exec &> /dev/null
+fi
+time make exec
+kill "$kserver_pid"
+
+"$kserver" &> /dev/null &
+kserver_pid=$!
+if [[ "$fast" == "false" ]]; then
+    # Warmup
+    make typecheck &> /dev/null
+fi
+time make typecheck
+kill "$kserver_pid"
+
+"$kserver" &> /dev/null &
+kserver_pid=$!
+if [[ "$fast" == "false" ]]; then
+    # Warmup
+    make compile &> /dev/null
+fi
+time make compile
+kill "$kserver_pid"
+
