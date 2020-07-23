158e599543357507f94189f0d82504ca298cbe2a
Reed Oei <oei.reed@gmail.com>
2020-07-23 12:19:12 -0500

    Cleanup, add basic ERC-721 description.

--- formalization/splash20-examples/erc20-transfer.sol
+++ formalization/splash20-examples/erc20-transfer.sol
@@ -3,7 +3,6 @@ contract EIP20 {
     function transfer(address to, uint256 value)
         public returns (bool) {
         require(value <= _balances[msg.sender]);
-        require(to != address(0));
         _balances[msg.sender] = _balances[msg.sender].sub(value);
         _balances[to] = _balances[to].add(value);
         return true;
