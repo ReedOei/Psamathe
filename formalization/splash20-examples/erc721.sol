e9c78f2dd2947d1707eccbd0554f447e907799af
Reed Oei <oei.reed@gmail.com>
2020-07-23 04:32:37 -0500

    Attempt to clean up ERC-721 example.

--- formalization/splash20-examples/erc721.sol
+++ formalization/splash20-examples/erc721.sol
@@ -3,30 +3,26 @@ contract NFToken {
   mapping (uint256 => address) idToApproval;
   mapping (address => uint256) ownerToNFTokenCount;
   mapping (address => mapping (address => bool)) ownerToOperators;
-  modifier canTransfer(uint256 _tokenId) {
-    address tokenOwner = idToOwner[_tokenId];
+  modifier canTransfer(uint256 tokId) {
+    address tokenOwner = idToOwner[tokId];
     require(tokenOwner == msg.sender ||
-            idToApproval[_tokenId] == msg.sender ||
-            ownerToOperators[tokenOwner][msg.sender],
-            NOT_OWNER_APPROWED_OR_OPERATOR);
+            idToApproval[tokId] == msg.sender ||
+            ownerToOperators[tokenOwner][msg.sender]);
     _;
   }
-  modifier validNFToken(uint256 _tokenId) {
-    require(idToOwner[_tokenId] != address(0), NOT_VALID_NFT);
-    _;
-  }
-  function transferFrom(address _from, address _to, uint256 _tokenId)
-    external override canTransfer(_tokenId) validNFToken(_tokenId) {
-    require(idToOwner[_tokenId] == _from, NOT_OWNER);
-    require(_to != address(0), ZERO_ADDRESS);
-    address from = idToOwner[_tokenId];
-    if (idToApproval[_tokenId] != address(0)) {
-      delete idToApproval[_tokenId];
+  function transferFrom(address _from, address _to, uint256 tokId)
+    external canTransfer(tokId) {
+    require(idToOwner[tokId] != address(0));
+    require(idToOwner[tokId] == _from);
+    require(_to != address(0));
+    if (idToApproval[tokId] != address(0)) {
+      delete idToApproval[tokId];
     }
-    require(idToOwner[_tokenId] == _from, NOT_OWNER);
+    require(idToOwner[tokId] == _from);
     ownerToNFTokenCount[_from] = ownerToNFTokenCount[_from] - 1;
-    delete idToOwner[_tokenId];
-    require(idToOwner[_tokenId] == address(0), NFT_ALREADY_EXISTS);
-    idToOwner[_tokenId] = _to;
+    delete idToOwner[tokId];
+    require(idToOwner[tokId] == address(0));
+    idToOwner[tokId] = _to;
     ownerToNFTokenCount[_to] = ownerToNFTokenCount[_to].add(1);
+  }
 }
