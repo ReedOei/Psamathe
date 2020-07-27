97e58c204f3a152cf4226538ede0a1678d49de78
Reed Oei <oei.reed@gmail.com>
2020-07-27 11:26:01 -0500

    Progress on poster

--- formalization/splash20-examples/erc20-transfer.sol
+++ formalization/splash20-examples/erc20-transfer.sol
@@ -1,11 +1,11 @@
 contract EIP20 {
-    mapping (address => uint256) private _balances;
-    function transfer(address to, uint256 value)
-        public returns (bool) {
-        require(value <= _balances[msg.sender]);
-        _balances[msg.sender] = _balances[msg.sender].sub(value);
-        _balances[to] = _balances[to].add(value);
-        return true;
-    }
+  mapping (address => uint256) balances;
+  function transfer(address dst, uint256 amt)
+    public returns (bool) {
+    require(amt <= balances[msg.sender]);
+    balances[msg.sender] = balances[msg.sender].sub(amt);
+    balances[dst] = balances[dst].add(amt);
+    return true;
+  }
 }
 
