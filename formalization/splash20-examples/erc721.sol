2a3e562d54bce1c569c6e1553d8ba0af03edda10
Reed Oei <oei.reed@gmail.com>
2020-07-29 12:53:15 -0500

    Resolve notes, add missing citations, improve examples

--- formalization/splash20-examples/erc721.sol
+++ formalization/splash20-examples/erc721.sol
@@ -4,23 +4,24 @@ contract NFToken {
   mapping (address => uint256) ownerToNFTokenCount;
   mapping (address => mapping (address => bool)) ownerToOperators;
 
+
   modifier canTransfer(uint256 tokId) {
-    require(tokenOwner == msg.sender ||
-            idToApproval[idToOwner[tokId]] == msg.sender ||
+    require(idToOwner[tokId] == msg.sender ||
+            idToApproval[tokId] == msg.sender ||
             ownerToOperators[idToOwner[tokId]][msg.sender]);
     _;
   }
-  function transferFrom(address _from, address _to, uint256 tokId)
+  function transferFrom(address src, address dst, uint256 tokId)
     external canTransfer(tokId) {
     require(idToOwner[tokId] != address(0));
-    require(idToOwner[tokId] == _from);
-    require(_to != address(0));
+    require(idToOwner[tokId] == src);
+    require(dst != address(0));
     if (idToApproval[tokId] != address(0)) {
       delete idToApproval[tokId];
     }
-    ownerToNFTokenCount[_from] = ownerToNFTokenCount[_from] - 1;
+    ownerToNFTokenCount[src] = ownerToNFTokenCount[src] - 1;
     delete idToOwner[tokId];
-    idToOwner[tokId] = _to;
-    ownerToNFTokenCount[_to] = ownerToNFTokenCount[_to].add(1);
+    idToOwner[tokId] = dst;
+    ownerToNFTokenCount[dst] = ownerToNFTokenCount[dst].add(1);
   }
 }
