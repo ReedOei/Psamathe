106e64291efbc62d58928d04d6eef5200226b966
Reed Oei <oei.reed@gmail.com>
2020-07-30 07:14:17 -0500

    Move example figures around...slightly better

--- formalization/splash20-examples/erc721.sol
+++ formalization/splash20-examples/erc721.sol
@@ -4,23 +4,16 @@ contract NFToken {
   mapping (address => uint256) ownerToNFTokenCount;
   mapping (address => mapping (address => bool)) ownerToOperators;
 
-
-  modifier canTransfer(uint256 tokId) {
+  function transferFrom(address src, address dst, uint256 tokId) external {
     require(idToOwner[tokId] == msg.sender ||
             idToApproval[tokId] == msg.sender ||
             ownerToOperators[idToOwner[tokId]][msg.sender]);
-    _;
-  }
-  function transferFrom(address src, address dst, uint256 tokId)
-    external canTransfer(tokId) {
     require(idToOwner[tokId] != address(0));
-    require(idToOwner[tokId] == src);
-    require(dst != address(0));
+    require(idToOwner[tokId] == src && dst != address(0));
     if (idToApproval[tokId] != address(0)) {
       delete idToApproval[tokId];
     }
     ownerToNFTokenCount[src] = ownerToNFTokenCount[src] - 1;
-    delete idToOwner[tokId];
     idToOwner[tokId] = dst;
     ownerToNFTokenCount[dst] = ownerToNFTokenCount[dst].add(1);
   }
