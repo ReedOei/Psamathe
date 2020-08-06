dbcda3836032dbee7a8699e7cf90e159c08b9fd3
Reed Oei <oei.reed@gmail.com>
2020-08-06 12:35:08 -0500

    Reformat code

--- formalization/splash20-examples/erc20-transfer.sol
+++ formalization/splash20-examples/erc20-transfer.sol
@@ -1,10 +1,10 @@
 contract ERC20 {
   mapping (address => uint256) balances;
-  function transfer(address dst, uint256 amt)
+  function transfer(address dst, uint256 amount)
     public returns (bool) {
-    require(amt <= balances[msg.sender]);
-    balances[msg.sender] = balances[msg.sender].sub(amt);
-    balances[dst] = balances[dst].add(amt);
+    require(amount <= balances[msg.sender]);
+    balances[msg.sender] = balances[msg.sender].sub(amount);
+    balances[dst] = balances[dst].add(amount);
     return true;
   }
 }
