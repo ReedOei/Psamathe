8bdf48300c30c3b3a1c128be0672d03ea9f949c0
Reed Oei <oei.reed@gmail.com>
2020-09-25 14:30:59 -0500

    Reorganization of examples for PADL

---
+++ formalization/padl-21-examples/erc20-transfer.sol
@@ -0,0 +1,12 @@
+contract ERC20 {
+  mapping (address => uint256) balances;
+  function transfer(address dst, uint256 amount)
+    public returns (bool) {
+    require(amount <= balances[msg.sender]);
+    balances[msg.sender] =
+      balances[msg.sender].sub(amount);
+    balances[dst] = balances[dst].add(amount);
+    return true;
+  }
+}
+
