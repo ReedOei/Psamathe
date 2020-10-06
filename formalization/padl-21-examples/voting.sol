5c21046721a072d9d8365fed05fc47ead52d74da
Reed Oei <oei.reed@gmail.com>
2020-10-06 12:31:37 -0500

    Add extended pdf

--- formalization/padl-21-examples/voting.sol
+++ formalization/padl-21-examples/voting.sol
@@ -1,16 +1,13 @@
 contract Ballot {
   struct Voter { uint weight; bool voted; uint vote; }
   struct Proposal { bytes32 name; uint voteCount; }
-
   address public chairperson;
   mapping(address => Voter) public voters;
   Proposal[] public proposals;
-
   function giveRightToVote(address voter) public {
     require(msg.sender == chairperson,
       "Only chairperson can give right to vote.");
-    require(!voters[voter].voted,
-      "The voter already voted.");
+    require(!voters[voter].voted, "The voter already voted.");
     voters[voter].weight = 1;
   }
   function vote(uint proposal) public {
