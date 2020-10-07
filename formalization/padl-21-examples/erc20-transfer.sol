7d99f3ac319a6e1f4f8177753daa0ea81b4caca8
Reed Oei <oei.reed@gmail.com>
2020-10-07 14:18:42 -0500

    Draft checkpoint for padl extended abstract

--- formalization/padl-21-examples/erc20-transfer.sol
+++ formalization/padl-21-examples/erc20-transfer.sol
@@ -1,9 +1,7 @@
-contract ERC20 {
-  mapping (address => uint256) balances;
-  function transfer(address dst, uint256 amount) public {
-    require(amount <= balances[msg.sender]);
-    balances[msg.sender] = balances[msg.sender].sub(amount);
-    balances[dst] = balances[dst].add(amount);
-  }
+mapping (address => uint256) balances;
+function transfer(address dst, uint256 amount) public {
+  require(amount <= balances[msg.sender]);
+  balances[msg.sender] = balances[msg.sender].sub(amount);
+  balances[dst] = balances[dst].add(amount);
 }
 
