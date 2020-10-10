57cb9d91b885c57ee97b415ccc4873d26d420793
Reed Oei <oei.reed@gmail.com>
2020-10-10 12:33:44 -0500

    Add more extensive macro expansion and type quant. infererence

--- scripts/save-test-output.sh
+++ scripts/save-test-output.sh
@@ -4,6 +4,11 @@ test_dir="$1"
 shift
 
 for fname in $(find "$test_dir" -name "*.flow" -type f); do
-    "$(git rev-parse --show-toplevel)/flow" "$@" "$fname" | tee "$fname.out"
+    FLAGS=()
+    if [[ -e "$fname.pat" ]]; then
+        FLAGS+=("--pattern")
+        FLAGS+=("$(cat "$fname.pat")")
+    fi
+    "$(git rev-parse --show-toplevel)/flow" "$@" "${FLAGS[@]}" "$fname" | tee "$fname.out"
 done
 
