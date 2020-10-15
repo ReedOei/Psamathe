79de1ad9f6102f79adbfe37f0413dac25a10904b
Reed Oei <oei.reed@gmail.com>
2020-10-15 14:04:56 -0500

    camera ready splash20 src

--- formalization/splash20-examples/erc20-transfer.sol
+++ formalization/splash20-examples/erc20-transfer.sol
@@ -1,12 +1,10 @@
 contract ERC20 {
-  mapping (address => uint256) balances;
-  function transfer(address dst, uint256 amount)
-    public returns (bool) {
+  mapping (address => uint) balances;
+  function transfer(address dst, uint amount) public {
     require(amount <= balances[msg.sender]);
     balances[msg.sender] =
       balances[msg.sender].sub(amount);
     balances[dst] = balances[dst].add(amount);
-    return true;
   }
 }
 
