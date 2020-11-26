d0382b86ac5600b11d623dd516ab144e12ea3db8
Reed Oei <oei.reed@gmail.com>
2020-11-25 21:10:50 -0600

    Add try catch

--- pure-compile.sh
+++ pure-compile.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+# output_file="compiled.sol"
 output_file="$(mktemp XXXXXXXXX.sol)"
-./flow pure-compile "$1" | tail -n+4 | head -n-1 | sed -E "s/(\.SolVarDefs|\.SolStmts|\.SolDecls|, \.Exprs|\.SolArgs)| ~> \.//g" |& tee "$output_file"
+./flow pure-compile --pattern "<k> Code </k>" "$1" \
+    | tail -n+4 \
+    | head -n-1 \
+    | sed -E "s/(\.SolVarDefs|\.SolStmts|\.SolDecls|, \.Exprs|, \.SolArgs)| ~> \.//g" \
+    | sed -E "s/ \.SolArgs//g" \
+    |& tee "$output_file"
 npx prettier "$output_file"
 
