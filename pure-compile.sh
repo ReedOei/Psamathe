03a2fbf081155e4615cc7a2c6bd4aa0999a9e825
Reed Oei <oei.reed@gmail.com>
2020-12-05 20:45:31 -0600

    Initial implementation of filters

--- pure-compile.sh
+++ pure-compile.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-output_file="compiled.sol"
-# output_file="$(mktemp XXXXXXXXX.sol)"
+input_file="$1"
+output_file="$(dirname "$input_file")/$(basename "$input_file" ".flow").sol"
+temp="$(mktemp XXXXXXXXX.sol)"
+
 ./flow pure-compile --pattern "<k> Code </k>" "$1" \
     | tail -n+4 \
     | head -n-1 \
     | sed -E "s/(\.SolVarDefs|\.SolStmts|\.SolDecls|, \.Exprs|, \.SolArgs)| ~> \.//g" \
     | sed -E "s/ \.SolArgs//g" \
     |& tee "$output_file"
-npx prettier "$output_file"
+npx prettier "$output_file" | tee "$temp"
+mv "$temp" "$output_file"
 
