95e83d9220cf8b16b0b5eabf1ed77cdcd0f2a86c
Reed Oei <oei.reed@gmail.com>
2021-01-11 14:33:44 -0600

    Finished padl 21 talk draft

--- papers/padl-21-examples/erc20-transfer.sol
+++
@@ -1,7 +0,0 @@
-mapping (address => uint256) balances;
-function transfer(address dst, uint256 amount) public {
-  require(amount <= balances[msg.sender]);
-  balances[msg.sender] = balances[msg.sender].sub(amount);
-  balances[dst] = balances[dst].add(amount);
-}
-
