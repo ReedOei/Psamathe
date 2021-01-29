8679092370e688fc9987433f234b68f17d8dd737
stackdynamic <stackdynamic@gmail.com>
2021-01-29 16:46:38 -0600

    Update Preprocessor functions to reflect Transform.hs changes

--- Compiler/src/Preprocessor.hs
+++ Compiler/src/Preprocessor.hs
@@ -34,22 +34,36 @@ preprocessDecl :: Decl Parsed-> State Env [Decl Preprocessed]
 preprocessDecl (TransformerDecl name args ret body) = do
     newBody <- concat <$> mapM preprocessStmt body
     modify $ set typeEnv Map.empty
-    pure [TransformerDecl name (map transformVarDef args) (transformVarDef ret) newBody]
-preprocessDecl d = pure [transformDecl d]
+    transformedArgs <- mapM transformVarDef args
+    transformedRet <- transformVarDef ret
+    pure [TransformerDecl name transformedArgs transformedRet newBody]
+
+preprocessDecl d = do
+    transformedDecl <- transformDecl d
+    pure [transformedDecl]
 
 -- TODO: Might need to make this more flexible, in case we need to generate declarations or something
 preprocessStmt :: Stmt Parsed -> State Env [Stmt Preprocessed]
 preprocessStmt (OnlyWhen cond) = preprocessCond cond
 preprocessStmt s@(Flow _ dst) = do
     declareVars dst
-    pure [transformStmt s]
+    transformedStmt <- transformStmt s
+    pure [transformedStmt]
+
 preprocessStmt s@(FlowTransform _ _ dst) = do
     declareVars dst
-    pure [transformStmt s]
-preprocessStmt s = pure [transformStmt s]
+    transformedStmt <- transformStmt s
+    pure [transformedStmt]
+
+preprocessStmt s = do
+    transformedStmt <- transformStmt s
+    pure [transformedStmt]
 
 declareVars :: Locator Parsed -> State Env ()
-declareVars (NewVar x t) = modify $ over typeEnv $ Map.insert x (transformBaseType t)
+declareVars (NewVar x baseT) = do
+    transformedBaseT <- transformBaseType baseT
+    modify $ over typeEnv $ Map.insert x transformedBaseT
+
 declareVars _ = pure ()
 
 preprocessCond :: Precondition Parsed -> State Env [Stmt Preprocessed]
@@ -60,7 +74,11 @@ preprocessCond (Disj (c1:cs)) = do
     checkC1 <- preprocessCond c1
     checkCs <- preprocessCond $ Disj cs
     pure [ Try checkC1 checkCs ]
-preprocessCond (NegateCond cond) = preprocessCond $ transformPrecondition (expandNegate cond)
+
+preprocessCond (NegateCond cond) = do
+    expandedCond <- expandNegate cond
+    preprocessCond expandedCond
+
 preprocessCond (BinOp op a b) = do
     (allocA, newA) <- allocateVar a
     newAllocA <- concat <$> mapM preprocessStmt allocA
@@ -69,29 +87,38 @@ preprocessCond (BinOp op a b) = do
     res <- expandCond $ BinOp op newA newB
     pure $ newAllocA ++ newAllocB ++ res
 
-expandNegate :: Precondition Parsed -> Precondition Preprocessed
-expandNegate (BinOp OpEq a b) = transformPrecondition (BinOp OpNe a b)
-expandNegate (BinOp OpNe a b) = transformPrecondition (BinOp OpEq a b)
-expandNegate (BinOp OpLt a b) = transformPrecondition (BinOp OpGe a b)
-expandNegate (BinOp OpGt a b) = transformPrecondition (BinOp OpLe a b)
-expandNegate (BinOp OpLe a b) = transformPrecondition (BinOp OpGt a b)
-expandNegate (BinOp OpGe a b) = transformPrecondition (BinOp OpLt a b)
-expandNegate (BinOp OpIn a b) = transformPrecondition (BinOp OpNotIn a b)
-expandNegate (BinOp OpNotIn a b) = transformPrecondition (BinOp OpIn a b)
-expandNegate (Conj conds) = Disj $ map expandNegate conds
-expandNegate (Disj conds) = Conj $ map expandNegate conds
+expandNegate :: Precondition Parsed -> State Env (Precondition Parsed)
+expandNegate (BinOp OpEq a b) = pure (BinOp OpNe a b)
+expandNegate (BinOp OpNe a b) = pure (BinOp OpEq a b)
+expandNegate (BinOp OpLt a b) = pure (BinOp OpGe a b)
+expandNegate (BinOp OpGt a b) = pure (BinOp OpLe a b)
+expandNegate (BinOp OpLe a b) = pure (BinOp OpGt a b)
+expandNegate (BinOp OpGe a b) = pure (BinOp OpLt a b)
+expandNegate (BinOp OpIn a b) = pure (BinOp OpNotIn a b)
+expandNegate (BinOp OpNotIn a b) = pure (BinOp OpIn a b)
+expandNegate (Conj conds) = do
+    expandedConds <- mapM expandNegate conds
+    pure $ Disj expandedConds
+
+expandNegate (Disj conds) = do
+    expandedConds <- mapM expandNegate conds
+    pure $ Conj expandedConds
+
 expandNegate (NegateCond cond) = transformPrecondition cond
 
 allocateVar :: Locator Parsed -> State Env ([Stmt Parsed], Locator Parsed)
 allocateVar (IntConst n) = do
     x <- freshName
     pure ([ Flow (IntConst n) (NewVar x Nat) ], Var x)
+
 allocateVar (BoolConst b) = do
     x <- freshName
     pure ([ Flow (BoolConst b) (NewVar x PsaBool) ], Var x)
+
 allocateVar l@(Multiset elemT elems) = do
     x <- freshName
     pure ([ Flow l (NewVar x (Table [] elemT))], Var x)
+
 allocateVar l = pure ([], l)
 
 expandCond :: Precondition Parsed -> State Env [Stmt Preprocessed]
@@ -105,21 +132,34 @@ expandCond :: Precondition Parsed -> State Env [Stmt Preprocessed]
 expandCond cond@(BinOp OpLt a b) = do
     addPreprocessorError $ UnimplementedError "only when" $ show cond
     pure []
+
 expandCond cond@(BinOp OpGt a b) = do
     addPreprocessorError $ UnimplementedError "only when" $ show cond
     pure []
-expandCond (BinOp OpEq a b) = pure [
-                                Flow (Select (transformLocator a) (transformLocator b)) (transformLocator a),
-                                Flow (Select (transformLocator b) (transformLocator a)) (transformLocator b)
-                            ]
+
+expandCond (BinOp OpEq a b) = do
+    transformedA <- transformLocator a
+    transformedB <- transformLocator b
+    pure [ Flow (Select transformedA transformedB) transformedA,
+           Flow (Select transformedB transformedA) transformedB ]
+
 expandCond (BinOp OpIn a b) = do
     tyA <- typeOfLoc a
+    qt <- transformXType (Infer tyA)
+    transformedA <- transformLocator a
+    transformedB <- transformLocator b
     --- TODO: Once we have type quantity inference, we can replace Any here with something more specific. Regardless, this should always be safe.
-    pure [ Flow (Select (transformLocator b) (Multiset (Any, tyA) [transformLocator a])) (transformLocator b) ]
+    pure [ Flow (Select transformedB (Multiset qt [transformedA])) transformedB ]
+
 expandCond (BinOp OpNe a b) = do
     failure <- expandCond (BinOp OpEq a b)
     pure [Try (failure ++ [ Revert ]) []]
-expandCond (BinOp OpLe a b) = pure [ Flow (Select (transformLocator b) (transformLocator a)) (transformLocator b) ]
+
+expandCond (BinOp OpLe a b) = do
+    transformedA <- transformLocator a
+    transformedB <- transformLocator b
+    pure [ Flow (Select transformedB transformedA) transformedB ]
+    
 expandCond (BinOp OpGe a b) = expandCond (BinOp OpLt b a)
 expandCond (BinOp OpNotIn a b) = do
     failure <- expandCond (BinOp OpIn a b)
