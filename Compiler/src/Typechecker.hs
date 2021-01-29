7333d8eb2f71241b55ba8333eec9512fa096a1df
stackdynamic <stackdynamic@gmail.com>
2021-01-29 16:57:21 -0600

    Update Typechecker functions to reflect Transform.hs changes

--- Compiler/src/Typechecker.hs
+++ Compiler/src/Typechecker.hs
@@ -8,6 +8,7 @@ import Control.Monad.State
 
 import AST
 import Env
+import Transform
 
 instance ProgramTransform Preprocessed Typechecked where
     transformXType = transformQuantifiedType
@@ -19,7 +20,10 @@ typecheck prog@(Program decls stmts) = do
     pure $ Program checkedDecls checkedStmts
 
 checkDecl :: Decl Preprocessed -> State Env (Decl Typechecked)
-checkDecl decl = pure $ transformDecl decl
+checkDecl = transformDecl
 
 checkStmt :: Stmt Preprocessed -> State Env (Stmt Typechecked)
-checkStmt (Flow src dst) = pure $ Flow (transformLocator src) (transformLocator dst)
+checkStmt (Flow src dst) = do
+    transformedSource <- transformLocator src
+    transformedDest <- transformLocator dst
+    pure $ Flow transformedSource transformedDest
